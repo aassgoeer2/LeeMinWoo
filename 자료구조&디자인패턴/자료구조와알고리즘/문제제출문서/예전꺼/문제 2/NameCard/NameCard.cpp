317d9e39940e5a27b125b770add783647d80bb49
aassgoeer2 <alsdn4680@naver.com>
2019-07-16 17:06:21 +0900

    LinkedRead

---
+++ 자료구조&디자인패턴/자료구조와알고리즘/문제제출문서/예전꺼/문제 2/NameCard/NameCard.cpp
@@ -0,0 +1,38 @@
+#include "NameCard.h"
+
+NameCard * MakeNameCard(char * name, char * phone)
+{
+
+	NameCard *Card = new NameCard;
+	for (int i = 0; i < 30; i++)
+	{
+		Card->name[i] = name[i];
+		Card->phone[i] = phone[i];
+	}
+
+	return Card;
+}
+
+void ShowNameCardInfo(NameCard * pcard)
+{
+	printf("이름 : %s\n", pcard->name);
+	printf("폰번호 : %s\n", pcard->phone);
+}
+
+int NameCompare(NameCard * pcard, char * name)
+{
+	for (int i = 0; i < 30; i++)
+	{
+		if (pcard->name[i] != name[i])
+			return 0;
+	}
+	return 1;
+}
+
+void ChangePhoneNum(NameCard * pcard, char * phone)
+{
+	for (int i = 0; i < 30; i++)
+	{
+		pcard->phone[i] = phone[i];
+	}
+}
